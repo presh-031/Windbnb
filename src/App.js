74be44d7fb334221063d461e96b9c95c20e2246c
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-05 10:20:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
