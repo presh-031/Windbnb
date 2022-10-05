714f62a29128de9aee5b33233772cdf5262997de
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-05 15:02:44 +0100

    form display logic now functional

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,30 @@
 import { MdSearch } from "react-icons/md";
 import { CgClose } from "react-icons/cg";
 
+import { useState } from "react";
+
 const Form = () => {
+  const [mainFormIsOpen, setMainFormIsOpen] = useState(false);
   const mainForm = (
-    <div>
+    <div className="border border-black absolute top-0 w-full bg-white z-10 h-32">
       <div>
         <p>Edit your search</p>
-        <CgClose />
+        <CgClose
+          onClick={() => {
+            setMainFormIsOpen(false);
+          }}
+        />
       </div>
     </div>
   );
   return (
     <>
-      <section className="border cursor-pointer  text-sm flex w-fit mx-auto mt-6">
+      <section
+        onClick={() => {
+          setMainFormIsOpen(true);
+        }}
+        className="border cursor-pointer  text-sm flex w-fit mx-auto mt-6"
+      >
         <div className="border px-4 py-2 ">
           <p className="text-[#BDBDBD]">Add location</p>
         </div>
@@ -23,6 +35,7 @@ const Form = () => {
           <MdSearch color="#EB5757" size="1.8rem" className="" />
         </div>
       </section>
+      {mainFormIsOpen && mainForm}
     </>
   );
 };
