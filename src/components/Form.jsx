5fbad3ce386e324735a452df4ab49b29025ccd5d
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-08 18:46:36 +0100

    onto adding functionality for form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,48 +7,9 @@ import { useState } from "react";
 const Form = () => {
   const [mainFormIsOpen, setMainFormIsOpen] = useState(false);
 
-  const mainForm = (
-    <div className="border p-[1.125rem]  border-black absolute top-0 w-full bg-white z-10">
-      <div className="flex justify-between border items-center ">
-        <p className="font-bold text-[0.75rem]">Edit your search</p>
-        <CgClose
-          onClick={() => {
-            setMainFormIsOpen(false);
-          }}
-        />
-      </div>
-      <div className=" mt-[1rem]">
-        <div className="border py-[0.6875rem] px-[1.625rem]">
-          <p className="text-[0.5625rem] font-bold">LOCATION</p>
-          <p className="text-[0.875rem] text-[#bdbdbd]">Add location</p>
-        </div>
-        <div className="border py-[0.6875rem] px-[1.625rem]">
-          <p className="text-[0.5625rem] font-bold">GUESTS</p>
-          <p className="text-[0.875rem] text-[#bdbdbd]">Add guests</p>
-        </div>
-      </div>
-      <div className="flex flex-col mb-[11.375rem] mt-[2.25rem] ml-[1.75rem]  gap-y-[2.25rem]">
-        <p className="flex items-center gap-x-[0.625rem] text-[0.875rem]">
-          <FaMapMarkerAlt /> Helsinki, Finland
-        </p>
-        <p className="flex items-center gap-x-[0.625rem] text-[0.875rem]">
-          <FaMapMarkerAlt /> Turku, Finland
-        </p>
-        <p className="flex items-center gap-x-[0.625rem] text-[0.875rem]">
-          <FaMapMarkerAlt /> Oulu, Finland
-        </p>
-        <p className="flex items-center gap-x-[0.625rem] text-[0.875rem]">
-          <FaMapMarkerAlt /> Vaasa, Finland
-        </p>
-      </div>
-      <div className="text-center">
-        <button className="rounded-[1rem] px-[1.5rem] py-[0.9375rem] bg-[#eb5757] text-[#f2f2f2] mb-[0.75rem] mx-auto border flex items-center gap-x-[0.6844rem]">
-          <MdSearch />
-          Search
-        </button>
-      </div>
-    </div>
-  );
+  function handleClick(e) {
+    console.log(e.target.innerText);
+  }
   return (
     <>
       <section
@@ -67,7 +28,49 @@ const Form = () => {
           <MdSearch color="#EB5757" size="1.8rem" className="" />
         </div>
       </section>
-      {mainFormIsOpen && mainForm}
+      {mainFormIsOpen && (
+        <div className=" p-[1.125rem]   absolute top-0 w-full bg-white z-10">
+          <div className="flex font-bold text-[0.75rem] justify-between  items-center ">
+            Edit your search
+            <CgClose
+              size={20}
+              onClick={() => {
+                setMainFormIsOpen(false);
+              }}
+            />
+          </div>
+          <div className=" mt-[1rem] rounded-[1rem] shadow-[0_1px_6px_rgba(0,0,0,0.1)]">
+            <div className=" py-[0.6875rem] px-[1.625rem]  border-b">
+              <p className="text-[0.5625rem] font-bold">LOCATION</p>
+              <p className="text-[0.875rem] text-[#bdbdbd]">Add location</p>
+            </div>
+            <div className=" py-[0.6875rem] px-[1.625rem]">
+              <p className="text-[0.5625rem] font-bold">GUESTS</p>
+              <p className="text-[0.875rem] text-[#bdbdbd]">Add guests</p>
+            </div>
+          </div>
+          <div className="flex flex-col mb-[11.375rem] mt-[2.25rem] ml-[1.75rem]  gap-y-[2.25rem]">
+            <div className="flex items-center gap-x-[0.625rem] text-[0.875rem]">
+              <FaMapMarkerAlt /> <p onClick={handleClick}>Helsinki, Finland</p>
+            </div>
+            <div className="flex items-center gap-x-[0.625rem] text-[0.875rem]">
+              <FaMapMarkerAlt /> <p onClick={handleClick}>Turku, Finland</p>
+            </div>
+            <div className="flex items-center gap-x-[0.625rem] text-[0.875rem]">
+              <FaMapMarkerAlt /> <p onClick={handleClick}>Oulu, Finland</p>
+            </div>
+            <div className="flex items-center gap-x-[0.625rem] text-[0.875rem]">
+              <FaMapMarkerAlt /> <p onClick={handleClick}>Vaasa, Finland</p>
+            </div>
+          </div>
+          <div className="text-center">
+            <button className="rounded-[1rem] px-[1.5rem] py-[0.9375rem] bg-[#eb5757] text-[#f2f2f2] mb-[0.75rem] mx-auto border flex items-center gap-x-[0.6844rem]">
+              <MdSearch />
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
