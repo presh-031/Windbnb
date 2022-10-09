336568007c4a8117ac5f86100ce64c01079136ca
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-10 00:06:01 +0100

    now using data locally as it is

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 
+import Stays from "./components/Stays";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import Apartment from "./components/Apartment";
-import Stays from "./components/Stays";
+
+import { staysdata } from "./data/staysdata";
 
 const App = () => {
-  const [apartments, setApartments] = useState(null);
-  const [staysNum, setStaysNum] = useState(null);
-  const [locations, setLocations] = useState(null);
-  function getAllApartments() {
-    axios
-      .get("/data/stays.json")
-      .then((response) => {
-        const data = response.data;
+  // const [apartments, setApartments] = useState(null);
+  // const [staysNum, setStaysNum] = useState(null);
+  // const [locations, setLocations] = useState(null);
+  // function getAllApartments() {
+  //   axios
+  //     .get("/data/stays.json")
+  //     .then((response) => {
+  //       const data = response.data;
 
-        setApartments(
-          data.map((apartment) => {
-            return <Apartment key={apartment.key} data={apartment} />;
-          })
-        );
-        setStaysNum(data.length);
-        setLocations(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  useEffect(() => {
-    getAllApartments();
-  }, []);
+  // setApartments(
+  //   staysdata.map((apartment) => {
+  //     return <Apartment key={apartment.key} data={apartment} />;
+  //   })
+  // );
+  // setStaysNum(staysdata.length);
+  // setLocations(staysdata);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
+  // useEffect(() => {
+  //   getAllApartments();
+  // }, []);
 
   return (
     <>
       <Header />
-      {locations && <Form />}
-      <Stays staysNum={staysNum} />
-      <div>{apartments}</div>
+      <Form />
+      <Stays staysNum={staysdata.length} />
+      <div>
+        {staysdata.map((apartment) => {
+          return <Apartment key={apartment.key} data={apartment} />;
+        })}
+      </div>
     </>
   );
 };
